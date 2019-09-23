6fc951442029e6c0d64a63258f40d7cabb58a435
Marco Edenhofer <edenhofermarco@t-online.de>
2019-09-23 20:42:01 +0200

    Implemented Template Method design pattern for GET requests; implemented JSON request parameter passing

--- server/openvas_api/connector.py
+++ server/openvas_api/connector.py
@@ -9,29 +9,30 @@ class OpenvasConnector:
     and abstracts the api calls.
     """
 
-    def __init__(self):
-        socket_path = '/var/run/openvasmd.sock'
-        conn = UnixSocketConnection(path=socket_path)
+    def __init__(self, *, socket_path='/var/run/openvasmd.sock', username='admin', password='admin'):
+        self.username = username
+        self.password = password
+        connection = UnixSocketConnection(path=socket_path)
         transform = EtreeCheckCommandTransform()
-        self.gmp = Gmp(connection=conn, transform=transform)
+        self.gmp = Gmp(connection=connection, transform=transform)
 
-    def __authenticate__(self):
-        with self.gmp:
-            self.gmp.authenticate(username='admin', password='admin')
+    def __authenticate__(self, gmp):
+        gmp.authenticate(username=self.username, password=self.password)
 
     def get_version(self):
-        with self.gmp:
-            return etree.tostring(self.gmp.get_version(), pretty_print=True)
+        with self.gmp as gmp:
+            return etree.tostring(gmp.get_version(), pretty_print=True)
 
-    def get_scanners(self):
-        with self.gmp:
-            self.gmp.authenticate(username='admin', password='admin')
-            return etree.tostring(self.gmp.get_scanners())
+    def get_scanner(self, scanner_id):
+        with self.gmp as gmp:
+            self.__authenticate__(gmp)
+            return etree.tostring(gmp.get_scanner(scanner_id))
 
-    def get_scanner_by_name(self, scanner_name):
-        with self.gmp:
-            self.gmp.authenticate(username='admin', password='admin')
-            return etree.tostring(self.gmp.get_scanners(filter=scanner_name))
+    def get_scanners(self, *, filter=None, filter_id=None, trash=None, details=None):
+        with self.gmp as gmp:
+            self.__authenticate__(gmp)
+            return etree.tostring(gmp.get_scanners(filter=filter, filter_id=filter_id, trash=trash, details=details))
+
 
     def random_shit(self):
             print(self.gmp.authenticate('admin', 'admin'))
--- server/run.py
+++ server/run.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from flask import Response
+from flask import Response, request
 from server.openvas_api.connector import OpenvasConnector
 
 app = Flask(__name__)
@@ -8,31 +8,91 @@ api = Api(app)
 
 openvas = OpenvasConnector()
 
-class AliveTest(Resource):
+class GetRequestTemplate(Resource):
+    """
+    This is a implementation of the Template Method Design Pattern.
+    This class provides a basic execution sequence, usually the extending class only needs to implement the function execute_api_call().
+    """ 
+
+    def setup(self):
+        self.request_body = request.get_json()
+        self.mimetype = 'application/xml'
+        self.api_response = "NOT IMPLEMENTED"
+
+    def execute_api_call(self):
+        pass
+
+    def return_response(self):
+        return Response(response=self.api_response, mimetype=self.mimetype)
+
     def get(self):
-        api_response = openvas.get_version()
-
-        res = Response(response=api_response, mimetype='application/xml')
-        # res.headers['Content-Type'] = "text/xml; charset=utf-8"
-        return res
+        self.setup()
+        self.api_response = self.execute_api_call()
+        return self.return_response()
 
 
-class Scanners(Resource):
-    def get(self):
-        api_response = openvas.get_scanners()
+class AliveTest(GetRequestTemplate):
+    def execute_api_call(self):
+        return openvas.get_version()
 
-        res = Response(response=api_response, mimetype='application/xml')
-        # res.headers['Content-Type'] = "text/xml; charset=utf-8"
-        return res
 
-class ScannerByName(Resource):
-    def get(self, scanner_name):
-        api_response = openvas.get_scanner_by_name(scanner_name)
+class Scanners(GetRequestTemplate):
+    def execute_api_call(self):
+        filter = get_filter_from_json(self.request_body)
+        filter_id = get_filter_id_from_json(self.request_body)
+        trash = get_trash_from_json(self.request_body)
+        details = get_details_from_json(self.request_body)
 
-        res = Response(response=api_response, mimetype='application/xml')
-        return res
+        return openvas.get_scanners(filter=filter, filter_id=filter_id, trash=trash, details=details)
+
+
+class ScannerById(GetRequestTemplate):
+    def execute_api_call(self):
+        scanner_id = get_scanner_id_from_json(self.request_body)
+
+        return openvas.get_scanner(scanner_id)
+
 
 api.add_resource(AliveTest, '/_alive')
+api.add_resource(ScannerById, '/_scanner/id')
 api.add_resource(Scanners, '/_scanners')
-api.add_resource(ScannerByName, '/_scanners/name/<scanner_name>')
 
+def get_filter_from_json(json_data):
+    name = json_data.get('filter')
+
+    if name != None and len(name) > 0:
+        return name
+    else:
+        return None
+
+def get_filter_id_from_json(json_data):
+    filter_id = json_data.get('filter_id')
+
+    if filter_id != None and len(filter_id) > 0:
+        return filter_id
+    else:
+        return None
+
+def get_trash_from_json(json_data):
+    trash = json_data.get('trash')
+
+    if trash != None and (trash == 'True' or trash == 'False'):
+        return trash
+    else:
+        return None
+
+def get_details_from_json(json_data):
+    details = json_data.get('details')
+
+    if details != None and (details == 'True' or details == 'False'):
+        return details
+    else:
+        return None
+
+def get_scanner_id_from_json(json_data):
+    scanner_id = json_data.get('scanner_id')
+
+    if scanner_id != None and len(scanner_id) > 0:
+        return scanner_id
+    else:
+        return None
